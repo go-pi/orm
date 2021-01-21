97bb6b652b1d427769d7a82f98a7e9da0aa243fe
Ryan <ruifafan@gmail.com>
2021-01-21 14:52:57 +0800

    更新数据库支持驱动类型

--- driver/driver.go
+++ driver/driver.go
@@ -1,11 +1,25 @@
 package driver
 
 // DIALECTOR support mysql/sqlite/postgres/mssql
-type DIALECTOR string
+type DIALECTOR uint
 
 const (
-	MySQL    = DIALECTOR("mysql")
-	SQLite   = DIALECTOR("sqlite")
-	Postgres = DIALECTOR("postgres")
-	MSSQL    = DIALECTOR("mssql")
+	MySQL    DIALECTOR = 1
+	SQLite   DIALECTOR = 2
+	Postgres DIALECTOR = 3
+	MSSQL    DIALECTOR = 4
 )
+
+func (d DIALECTOR) String() string {
+	switch d {
+	case MySQL:
+		return "mysql"
+	case SQLite:
+		return "sqlite"
+	case Postgres:
+		return "postgres"
+	case MSSQL:
+		return "mssql"
+	}
+	return "unknown"
+}
