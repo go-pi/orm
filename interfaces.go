8feddd75c0f0eacfa5b8aabd3e68580498ba2434
Ryan <ruifafan@gmail.com>
2021-01-21 09:53:35 +0800

    Added interface of ConnPool

---
+++ interfaces.go
@@ -0,0 +1,14 @@
+package orm
+
+import (
+	"context"
+	"database/sql"
+)
+
+// ConnPool db conns pool interface
+type ConnPool interface {
+	PrepareContext(ctx context.Context, query string) (*sql.Stmt, error)
+	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
+	QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error)
+	QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row
+}
